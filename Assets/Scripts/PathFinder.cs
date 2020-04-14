1158dd25a1c44bedf0d395cffbaa9ffaf35199e5
pab15 <54556870+pab15@users.noreply.github.com>
2020-04-14 16:35:09 -0700

    Forgot to create repository

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PathFinder
+{
+    public static TilePath DiscoverPath(Tilemap map, Vector3Int start, Vector3Int end)
+    {
+        //you will return this path to the user.  It should be the shortest path between
+        //the start and end vertices 
+        TilePath discoveredPath = new TilePath();
+
+        //TileFactory is how you get information on tiles that exist at a particular vector's
+        //coordinates
+        TileFactory tileFactory = TileFactory.GetInstance();
+
+        //This is the priority queue of paths that you will use in your implementation of
+        //Dijkstra's algorithm
+        PriortyQueue<TilePath> pathQueue = new PriortyQueue<TilePath>();
+
+        //You can slightly speed up your algorithm by remembering previously visited tiles.
+        //This isn't strictly necessary.
+        Dictionary<Vector3Int, int> discoveredTiles = new Dictionary<Vector3Int, int>();
+
+        //quick sanity check
+        if(map == null || start == null || end == null)
+        {
+            return discoveredPath;
+        }
+
+        //This is how you get tile information for a particular map location
+        //This gets the Unity tile, which contains a coordinate (.Position)
+        var startingMapLocation = map.GetTile(start);
+
+        //And this converts the Unity tile into an object model that tracks the
+        //cost to visit the tile.
+        var startingTile = tileFactory.GetTile(startingMapLocation.name);
+        startingTile.Position = start;
+
+        //Any discovered path must start at the origin!
+        discoveredPath.AddTileToPath(startingTile);
+
+        //This adds the starting tile to the PQ and we start off from there...
+        pathQueue.Enqueue(discoveredPath);
+        bool found = false;
+
+        int count = 0;
+        while(found == false && pathQueue.IsEmpty() == false)
+        {
+            //TODO: Implement Dijkstra's algorithm!
+
+            // Dequeue Top, 
+            TilePath current_path = new TilePath(pathQueue.GetFirst());
+            pathQueue.Dequeue();
+            Tile current_tile = new Tile(current_path.GetMostRecentTile());
+            Vector3Int current_position = new Vector3Int(current_tile.Position.x, current_tile.Position.y, current_tile.Position.z);
+
+            List<Vector3Int> adjacent_tiles = new List<Vector3Int>();
+            adjacent_tiles.Add(new Vector3Int(current_position.x, current_position.y + 1, current_position.z));
+            adjacent_tiles.Add(new Vector3Int(current_position.x, current_position.y - 1, current_position.z));
+            adjacent_tiles.Add(new Vector3Int(current_position.x + 1, current_position.y, current_position.z));
+            adjacent_tiles.Add(new Vector3Int(current_position.x - 1, current_position.y, current_position.z));
+            for (int i = 0; i < adjacent_tiles.Count; i++)
+            {
+                TilePath new_path = new TilePath(current_path);
+                Vector3Int neighbor_position = new Vector3Int(adjacent_tiles[i].x, adjacent_tiles[i].y, adjacent_tiles[i].z);
+                //Get Neighbor node and set equal to visited_node
+
+                var visited_node_location = map.GetTile(neighbor_position);
+                if (visited_node_location == null)
+                {
+                    continue;
+                }
+                var visited_node = tileFactory.GetTile(visited_node_location.name);
+                visited_node.Position = neighbor_position;
+
+                if (visited_node.Position == end)
+                {
+                    new_path.AddTileToPath(visited_node);
+                    discoveredPath = new_path;
+                    found = true;
+                    break;
+                }
+                else if (discoveredTiles.ContainsKey(neighbor_position) == false)
+                {
+                    new_path.AddTileToPath(visited_node);
+                    pathQueue.Enqueue(new_path);
+                    discoveredTiles.Add(neighbor_position, 1);
+                    count++;
+                }
+            }
+        }
+        //foreach (TilePath path in pathQueue)
+        //{
+        //    if (path.GetMostRecentTile().Position != end)
+        //    {
+        //        pathQueue.Dequeue()
+        //    }
+        //}
+        //discoveredPath = pathQueue.GetFirst();
+        //Debug.Log(discoveredPath.Weight);
+        return discoveredPath;
+    }
+}
---
+++ Assets/Scripts/TileFactory.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileFactory
+{
+    private Dictionary<string, Tile> _tiles = new Dictionary<string, Tile>();
+    private static TileFactory _instance = null;
+
+    private TileFactory()
+    {
+        _tiles.Add("OutdoorsGround_0", new Tile() { 
+            Name = "Grass",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_6", new Tile()
+        {
+            Name = "Road",
+            Weight = 5
+        });
+        _tiles.Add("OutdoorsGround_15", new Tile()
+        {
+            Name = "Rough_Road",
+            Weight = 25
+        });
+        _tiles.Add("OutdoorsGround_44", new Tile()
+        {
+            Name = "Lake",
+            Weight = 100
+        });
+        _tiles.Add("OutdoorsGround_29", new Tile()
+        {
+            Name = "Pond",
+            Weight = 50
+        });
+        _tiles.Add("OutdoorsGround_36", new Tile()
+        {
+            Name = "Pond_Edge",
+            Weight = 25
+        });
+        _tiles.Add("OutdoorsGround_38", new Tile()
+        {
+            Name = "Pond_Edge",
+            Weight = 25
+        });
+        _tiles.Add("OutdoorsGround_30", new Tile()
+        {
+            Name = "Pond_Edge",
+            Weight = 25
+        });
+        _tiles.Add("OutdoorsGround_32", new Tile()
+        {
+            Name = "Pond_Edge",
+            Weight = 25
+        });
+        _tiles.Add("OutdoorsGround_13", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_18", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_20", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_22", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_24", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("OutdoorsGround_25", new Tile()
+        {
+            Name = "Path_Edge",
+            Weight = 10
+        });
+        _tiles.Add("Generic", new Tile()
+        {
+            Name = "Unknown",
+            Weight = 10
+        });
+    }
+
+    public static TileFactory GetInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = new TileFactory();
+        }
+        return _instance;
+    }
+
+    public Tile GetTile(string tileName)
+    {
+        if (_tiles.ContainsKey(tileName))
+        {
+            return _tiles[tileName];
+        }
+        else
+        {
+            return _tiles["Generic"];
+        }
+    }
+}
